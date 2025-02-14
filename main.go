195e1c02572a6342761e6902c3028a88994fe781
uekann <kantaueno0808@yahoo.co.jp>
2025-02-14 23:15:20 +0900

    update

--- main.go
+++ main.go
@@ -2,11 +2,17 @@
 package main
 
 import (
-	// "regchecker/ast"
+	"encoding/json"
 	"fmt"
+	"regchecker/ast"
 )
 
 func main() {
-	// Hello World
-	fmt.Println("Hello World")
+	str := "a(b|c)*d"
+	tokenizer := ast.NewTokenizer(str)
+	tokens, _ := tokenizer.Tokenize()
+	parser := ast.NewParser(tokens)
+	ast, _ := parser.Parse()
+	astJSON, _ := json.Marshal(ast)
+	fmt.Println(string(astJSON))
 }
